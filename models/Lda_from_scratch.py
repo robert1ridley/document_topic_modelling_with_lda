709226fcd4b4b44053ead808f98d198e9b3a5e23
Robert Ridley <robert.n.ridley@gmail.com>
2018-12-29 20:14:09 +0800

    update lda

--- models/Lda_from_scratch.py
+++ models/Lda_from_scratch.py
@@ -7,11 +7,16 @@ import bisect
 
 
 class Lda(object):
-  def __init__(self):
+  def __init__(self, in_number_of_categories, in_number_of_passes):
     self.word_lookup = {'word_count': 0}
     self.documents = []
     self.alpha = 0.1
     self.beta = 0.0002
+    self.number_of_categories = in_number_of_categories
+    self.number_of_passes = in_number_of_passes
+    self.words_per_topic = 10
+    self.n_words = None
+    self.topic_and_words = None
 
 
   def word_counts(self, articles):
@@ -38,69 +43,75 @@ class Lda(object):
     return bisect.bisect_right(partials, choice)
 
 
-  def probs(self, v, nkm, nkr, nk, n_topics):
+  def probs(self, v, nkm, nkr, nk):
     n_words = self.word_lookup["word_count"]
-    res = [0] * n_topics
-    for k in range(n_topics):
+    res = [0] * self.number_of_categories
+    for k in range(self.number_of_categories):
       res[k] = (nkm[k] + self.alpha) * (nkr[k][v] + self.beta) / (nk[k] + n_words * self.beta)
     return res
 
 
-  def get_topics(self, iters, w_counts, docs, n_topics):
-    n_words = self.word_lookup["word_count"]
+  def get_topics(self, w_counts):
+    self.n_words = self.word_lookup["word_count"]
+    number_of_categories = self.number_of_categories
+    number_of_passes = self.number_of_passes
+    documents = self.documents
     zs = []
-    nkr = [[0] * n_words for _ in range(n_topics)]
-    nkm = [[0] * n_topics for _ in range(len(docs))]
-    nk = [0] * n_topics
+    nkr = [[0] * self.n_words for _ in range(number_of_categories)]
+    nkm = [[0] * number_of_categories for _ in range(len(documents))]
+    nk = [0] * number_of_categories
 
     n_words_total = 0
-    for i in range(len(docs)):
+    for i in range(len(documents)):
       zs.append([])
-      for j in range(len(docs[i])):
-        topic = random.randint(0, n_topics - 1)
+      for j in range(len(documents[i])):
+        topic = random.randint(0, number_of_categories - 1)
         zs[i].append(topic)
-        ind = docs[i][j]
+        ind = documents[i][j]
         nkm[i][topic] += 1
         nkr[topic][ind] += 1
         nk[topic] += 1
         n_words_total += 1
-    for it in range(iters):
-      print ("Iteration", it)
-      for i in range(len(docs)):
-        for j in range(len(docs[i])):
-          ind = docs[i][j]
+    for it in range(number_of_passes):
+      print ("Pass", it)
+      for i in range(len(documents)):
+        for j in range(len(documents[i])):
+          ind = documents[i][j]
           k = zs[i][j]
           nkm[i][k] -= 1
           nkr[k][ind] -= 1
           nk[k] -= 1
-          ps = self.probs(ind, nkm[i], nkr, nk, n_topics)
+          ps = self.probs(ind, nkm[i], nkr, nk)
           newk = self.random_choice(ps)
           nkm[i][newk] += 1
           nkr[newk][ind] += 1
           nk[newk] += 1
           zs[i][j] = newk
 
-    for k in range(n_topics):
-      for v in range(n_words):
+    for k in range(number_of_categories):
+      for v in range(self.n_words):
         nkr[k][v] /= nk[k] + 0.
         nkr[k][v] -= (w_counts[v] + 0.) / n_words_total
-    return [[nk[k], nkr[k]] for k in range(n_topics)]
+    self.topic_and_words = [[nk[k], nkr[k]] for k in range(number_of_categories)]
 
 
-  def display_topics(self, topics):
-    relevant = 10
+  def display_topics(self):
+    words_per_topic = self.words_per_topic
     n_words = self.word_lookup["word_count"]
-    for nk, t in topics:
-      top = [[-1, 0]] * relevant
+    topics_and_words = self.topic_and_words
+    topic_num = 1
+    for nk, t in topics_and_words:
+      topic = [[-1, 0]] * words_per_topic
       for i in range(n_words):
-        for j in range(relevant):
-          if t[i] > top[j][1]:
-            top[j + 1:] = top[j:-1]
-            top[j] = [self.word_lookup[i], t[i]]
+        for j in range(words_per_topic):
+          if t[i] > topic[j][1]:
+            topic[j + 1:] = topic[j:-1]
+            topic[j] = [self.word_lookup[i], t[i]]
             break
-      print ("\n" + "==TOPIC==", " with number of words =", nk)
-      for rank in top:
-        print (rank)
+      print ("\nTopic: {}".format(topic_num))
+      topic_num += 1
+      for word_prob_pair in topic:
+        print ('Word: "{}" Probability: {}'.format(word_prob_pair[0], word_prob_pair[1]))
 
 
 def tokenize_articles(articles):
@@ -113,21 +124,20 @@ def tokenize_articles(articles):
 
 def main(args):
   articles_filepath = args[0]
-  iterations = 10
+  iterations = 2
   number_of_categories = int(args[1])
   with open(articles_filepath, 'rb') as infile:
     articles = _pickle.load(infile)
   articles = tokenize_articles(articles)
-  lda = Lda()
+  lda = Lda(number_of_categories, iterations)
   lda.word_counts(articles)
   word_count = lda.word_lookup['word_count']
   count = [0]*word_count
   for document in lda.documents:
     for word in document:
       count[word] += 1
-  topics = lda.get_topics(iterations, count, lda.documents, number_of_categories)
-  lda.display_topics(topics)
-
+  lda.get_topics(count)
+  lda.display_topics()
 
 
 if __name__ == '__main__':
